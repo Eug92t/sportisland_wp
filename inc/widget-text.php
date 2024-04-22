8471eb6252201768aac5b261ba231919d35a29ac
Eugene Tsvetukhin <eug92t@gmail.com>
2024-04-22 23:32:58 +0300

    add class SI_Widget_Text

--- inc/widget-text.php
+++ inc/widget-text.php
@@ -1,5 +1,14 @@
 <?php
 class SI_Widget_Text extends WP_Widget {
-    
-}
+    public function __construct(){
+        parent::__construct(
+            'si_widget_text',
+            'SportIsland - текстовый виджет',
+            [
+                'name' => 'SportIsland - текстовый виджет',
+                'description' => 'Текст без вёрстки'
+            ]
+            );
+    }
+};
 ?>
